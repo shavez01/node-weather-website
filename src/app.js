3053c744e86c00e3f9f93a7a77f12dbdd95fbc4c
shavez <shvz.mohammad@gmail.com>
2019-05-18 21:02:42 +0530

    Add high and low temrature

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use(express.static(patDir));
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'weather look',
+        title: 'Weather',
         name: 'Shavez'
     })
 });
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitute, longitute, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
-            callback(undefined, `${body.daily.data[0].summary} It is Currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability} % chance of rain`);
+            callback(undefined, `${body.daily.data[0].summary} It is Currently ${body.currently.temperature} degrees out. This high today is ${body.daily.data[0].temperatureHigh} with a low of ${body.daily.data[0].temperatureLow}. There is a ${body.currently.precipProbability} % chance of rain`);
         }
     });
 }
